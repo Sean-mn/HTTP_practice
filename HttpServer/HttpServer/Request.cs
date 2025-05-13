34cccfbbaabe40ac2227cf114dc77eb52b24fa36
Sean-mn <b01080080329@gmail.com>
2025-05-13 11:44:09 +0900

    Request 클래스 구현

---
+++ HttpServer/HttpServer/Request.cs
@@ -0,0 +1,19 @@
+﻿namespace HttpServer;
+
+public class Request
+{
+    public string Method { get; set; }
+    public string Path { get; set; }
+
+    public static Request Parse(string req)
+    {
+        var lines = req.Split("\r\n");
+        var requestLine = lines[0].Split(' ');
+
+        return new Request
+        {
+            Method = requestLine[0],
+            Path = requestLine[1],
+        };
+    }
+}
